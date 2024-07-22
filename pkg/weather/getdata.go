a723f8cfe136b31919ca61df6ef99a3d98cf638a
IDKWhyIDoThat <rinatu1506@gmail.com>
2024-07-22 10:50:41 +0300

    @v2.0

--- pkg/weather/getdata.go
+++ pkg/weather/getdata.go
@@ -3,7 +3,7 @@ package weather
 func GetCityWeatherData(city string, outputformat int, valueformat int) (string, error) {
 	weatherData, err := GetWeather(replaceSpaces(city, '-'))
 	if err != nil {
-		return "Something gone terribly wrong", err
+		return "Something gone terribly wrong. Probably server went down. Please, try again later.", err
 	}
 	answer, err := WeatherDataOutputFormat(*weatherData, outputformat, valueformat)
 	if err != nil {
